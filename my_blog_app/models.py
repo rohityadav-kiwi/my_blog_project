c619d391531271a19ae6e5df3f00f2c168774a05
rohit <rohityadav01111@gmail.com>
2019-11-13 15:25:07 +0530

    Project setup and login and signup

---
+++ my_blog_app/models.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils import timezone
+
+
+class BlogPost(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    post_content = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+    is_published = models.BooleanField(default=False)
+    #
+    # def publish(self):
+    #     self.published_date = models.DateTimeField(timezone.now)
+    #     self.save()
+    #     return self.published_date
+
+    def __str__(self):
+        return self.title
